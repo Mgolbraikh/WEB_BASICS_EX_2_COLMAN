34ddfe60997813c777697b84c185cbb9e75a4df1
Michael Golbraikh <mgolbraikh@gmail.com>
2016-08-27 19:31:17 +0300

    commit first thing

---
+++ src/Ex_web_2/Models/Fan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_web_2.Models
+{
+    public class Fan
+    {
+        public int ID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        public String Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
+        public int YearsInClub { get; set; }
+    }
+}
